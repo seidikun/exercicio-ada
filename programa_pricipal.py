bdc6f8bffa3f6bba9d61d48b75f235c458251a37
Seidi Yonamine <seidi.yamauti@gmail.com>
2023-04-20 11:57:03 -0300

    Update programa_pricipal.py

--- programa_pricipal.py
+++ programa_pricipal.py
@@ -1,5 +1,17 @@
 # Chama as funções colaborativas e roda elas em sequência
+import time
+from programa1 import function1
+from programa2 import function2
+from programa3 import function3
+from programa4 import function4
 
 texto  = function1()
-
 texto2 = function2(texto)
+texto3 = function3(texto2)
+texto4 = function4(texto3)
+
+for paragrafo in texto4:
+  print(paragrafo)
+  time.sleep(5)
+  
+ print('É ISSO. FIM')
